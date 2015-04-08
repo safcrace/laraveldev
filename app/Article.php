3d3e13acf13f8265fb97bfbe4ef86d07c3ac8d78
Sender Flores <safcrace@gmail.com>
2015-04-08 04:09:13 +0000

    Lesson 14

--- app/Article.php
+++ app/Article.php
@@ -8,7 +8,8 @@ class Article extends Model {
 	protected $fillable = [
 		'title',
 		'body',
-		'published_at'
+		'published_at',
+		'user_id'
 	];
 
 	protected $dates = ['published_at'];
@@ -27,6 +28,11 @@ class Article extends Model {
 	public function setPublishedAtAttribute($date)
 	{
 		$this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date);
+	}
+
+	public function user()
+	{
+		return $this->belongto('App\User');
 	}
 
 }
