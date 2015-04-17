e19e8054a469e2c91df7e6a501913fe0d2959fb5
vagrant <vagrant@homestead>
2015-04-17 02:54:36 +0000

    Lesson 23

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,15 @@
 
 	<meta charset="utf-8">
 	
-	<title>Laravel</title>
+	<title>My New App</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/css/select2.min.css" />
+	
+
 </head>
 <body>
 	
@@ -18,14 +23,16 @@
 		@yield('content')	
 	</div>
 	
-	@yield('footer')
+	
 
-	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/js/select2.min.js"></script>
 	<script>
 		$('#flash-overlay-modal').modal();
 		//$('div.alert').not('.alert-important').delay(3000).slideUp(300);
 	</script>
+	@yield('footer')
 	
 </body>
 </html>
